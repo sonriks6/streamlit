008f5bc90b2d62b522ede9f77e4dcecb9436803a
Santiago Rodríguez Díaz <sonriks6@outlook.es>
2023-01-18 08:32:23 +0100

    0.02

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,16 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
 })
 
 df
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(dataframe.style.highlight_max(axis=0))
