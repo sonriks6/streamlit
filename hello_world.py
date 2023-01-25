860dbc44a237b9367b56658e7d3855898a01e202
Santiago Rodríguez Díaz <sonriks6@outlook.es>
2023-01-25 10:30:52 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv("State.csv")
+df = pd.read_csv("https://shiftup.sharepoint.com/:x:/r/sites/WildfireProjet/Shared%20Documents/General/Wildfire_cleaned_dataset_4.csv?d=wd0eb4bf3223a497ebcf5616557394609&csf=1&web=1&e=6nGLAs")
 df
