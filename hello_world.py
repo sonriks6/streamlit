b9ccb7692d4744f218623b15e6653cffc46faee9
Santiago Rodriguez <santiago.rodriguez4@stellantis.com>
2023-01-25 17:55:04 +0100

    up

--- hello_world.py
+++ hello_world.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.maxUploadSize = 500
+df = pd.read_csv("State.csv")
 
-df = pd.read_csv("https://cez.ynh.fr/nextcloud/s/myctfjPrWEsWZ7R/download/Wildfire_cleaned_dataset_2.csv")
-
-df
+df.head()
