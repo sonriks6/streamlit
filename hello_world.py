ac1c2512b524b2f7220ac21e382320df8f84bfea
Santiago Rodríguez Díaz <sonriks6@outlook.es>
2023-01-25 11:24:53 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -2,5 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+st.maxUploadSize = 500
+
 df = pd.read_csv("https://cez.ynh.fr/nextcloud/s/CLHfSTpKXXmwStn/download/Wildfire_cleaned_dataset_4.csv")
 df
