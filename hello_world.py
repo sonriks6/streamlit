70f9617124a0cba766858e448f5262549086729c
Santiago Rodríguez Díaz <sonriks6@outlook.es>
2023-01-25 19:39:23 +0100

    up

--- hello_world.py
+++ hello_world.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame(
-   np.random.randn(10, 20),
-   columns=('col %d' % i for i in range(20)))
+df = pd.read_csv("gs://streamlit-f40ff.appspot.com/Wildfire_cleaned_dataset_4.csv")
 
 st.dataframe(df.style.highlight_max(axis=0))
