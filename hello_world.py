2ad065eb1a56d6b4a8dddd0fa34208fff26ca593
Santiago Rodríguez Díaz <sonriks6@outlook.es>
2023-01-25 10:53:23 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv("https://docs.google.com/open?id=1OiSLrtLsZiBM7olh6Oyl6psNMskzIwvC&authuser=0")
+df = pd.read_csv("https://drive.google.com/uc?id=1OiSLrtLsZiBM7olh6Oyl6psNMskzIwvC&amp;export=download&amp;confirm=t&amp;uuid=f01ad7ef-26e5-437e-a9a1-ecc42860bc89&amp;at=ACjLJWnbqTYgU887F8qG6cCmcNnl:1674640247521")
 df
