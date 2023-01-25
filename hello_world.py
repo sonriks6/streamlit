a68aceaaaee17048d668552f86faab4028339734
Santiago Rodríguez Díaz <sonriks6@outlook.es>
2023-01-25 19:07:14 +0100

    up

--- hello_world.py
+++ hello_world.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv("State.csv")
+df = pd.DataFrame(
+   np.random.randn(10, 20),
+   columns=('col %d' % i for i in range(20)))
 
-df.head()
+st.dataframe(df.style.highlight_max(axis=0))
