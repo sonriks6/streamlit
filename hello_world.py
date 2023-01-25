2b06b3ee8c22f46f7ab766aabfdf355f2c7789f3
Santiago Rodríguez Díaz <sonriks6@outlook.es>
2023-01-25 11:41:07 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -4,5 +4,6 @@ import numpy as np
 
 st.maxUploadSize = 500
 
-df = pd.read_csv("https://cez.ynh.fr/nextcloud/s/CLHfSTpKXXmwStn/download/Wildfire_cleaned_dataset_4.csv")
+df = pd.read_csv("https://cez.ynh.fr/nextcloud/s/myctfjPrWEsWZ7R/download/Wildfire_cleaned_dataset_2.csv")
+
 df
