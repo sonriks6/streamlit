a53292f7ef7687b20c8e32df58880be34e9d3a11
Santiago Rodríguez Díaz <sonriks6@outlook.es>
2023-01-25 18:58:58 +0100

    up

--- hello_world.py
+++ hello_world.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv("State.csv")
+df = pd.read_csv("https://data.world/sonriks6/wildfires/workspace/file?filename=Wildfire_cleaned_dataset_4.csv")
 
 df.head()
