b564e701a83a7d2d2b206a4d927a148c66bda290
Santiago Rodríguez Díaz <sonriks6@outlook.es>
2023-02-09 19:41:33 +0100

    .

---
+++ pages/2_🌍_Map_Visualization.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+
+# Dashboard title
+st.title("Wildfire Dashboard DataViz")
+st.markdown("Here we show data insights collected from 1992 to 2015:")
+
+# Dataset we need to import
+DATA_URL = ("https://raw.githubusercontent.com/sonriks6/streamlit/main/wildfire_compressed.parquet")
+
+@st.cache(persist = True)
+def load_data():
+   data = pd.read_parquet(DATA_URL)
+   return data
+
+# Load entire dataset
+data = load_data()
+
+# plot a streamlit map
+st.header("Move the sliders to visualize fires per year and category [A - G]:")
+# plot the sliders
+year = st.slider("Year:", 1992, 2015)
+fire_class_options = ["A", "B", "C", "D", "E", "F", "G"]
+default_option=fire_class_options.index("A")
+fire_class = st.selectbox(":fire:", options=fire_class_options, index=default_option)
+st.map(data.query("(FIRE_YEAR == @year) & (FIRE_SIZE_CLASS == @fire_class)")[["LATITUDE", "LONGITUDE"]].dropna(how ="any"))
